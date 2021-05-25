cc193844b51602d4b0037d7af3db5d436a3d5029
Brady <hawk0120@algonquinlive.com>
2021-05-25 08:54:23 -0500

    Added PlayViewFragment

--- app/src/main/java/com/example/audiobookplayer/MainActivity.java
+++ app/src/main/java/com/example/audiobookplayer/MainActivity.java
@@ -9,6 +9,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            PlayViewFragment fragmentDemo = (PlayViewFragment)
+                    getSupportFragmentManager().findFragmentById(R.id.fragment_container_view);
+        }
     }
 }
